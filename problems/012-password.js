9484cd73302353ceea2b74fa571c02c7dd21b33a
ascent2009 <ascent2009@hotmail.com>
2023-04-10 00:19:08 +0300

    012-password solved

--- problems/012-password.js
+++ problems/012-password.js
@@ -18,7 +18,26 @@
  * @returns {boolean}
  */
 function validatePassword(password) {
-    return undefined;
+    let isValidPassword = false;
+    if (password.length < 7) {
+        isValidPassword;
+    } else {
+        let upperCase = false;
+        let numbers = false;
+        for (let i = 0; i < password.length; i++) {
+            let num = !isNaN(Number(password[i]));
+            if (password[i] === password[i].toUpperCase() && !num) {
+                upperCase = true;
+            }
+            if (num) {
+                numbers = true;
+            }
+        }
+        if (upperCase && numbers) {
+            isValidPassword = true;
+        }
+    }
+    return isValidPassword;
 }
 
 module.exports = validatePassword;
